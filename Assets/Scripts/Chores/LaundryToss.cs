92721d05938ab382a86758a89676a83a474ed90a
Daniel <danielfer@unicauca.edu.co>
2025-07-03 16:05:26 -0500

    The Chore basic scripts are added, this will allow to control the flow of each minigame

---
+++ Assets/Scripts/Chores/LaundryToss.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace Chores
+{
+    public class LaundryToss : Chore
+    {
+        protected override void StartTutorial()
+        {
+            Debug.Log("Starting tutorial for Example Chore");
+        }
+
+        public override void StarMiniGameChore()
+        {
+            Debug.Log("Starting Minigame for Example Chore");
+        }
+
+        public override void CompleteChore()
+        {
+            base.CompleteChore();
+            Debug.Log("Example Chore completed!");
+        }
+
+        public override void EndChore()
+        {
+            onChoreEnded.Invoke();
+            Debug.Log("Example Chore Ended!");
+        }
+    }
+}
