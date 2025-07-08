a9bd66caadf0e5cc3eecf9a8ed78f7a5b089fa4f
Daniel <danielfer@unicauca.edu.co>
2025-07-07 23:49:02 -0500

    PlacementObjectValidation is added to only move when the placemennt is successfull

--- Assets/Scripts/PlacementObjectValidation.cs
+++ Assets/Scripts/PlacementObjectValidation.cs
@@ -1,7 +1,27 @@
-namespace DefaultNamespace
+using Meta.XR.MRUtilityKit.BuildingBlocks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlacementObjectValidation : MonoBehaviour
 {
-    public class PlacementObjectValidation
+    [SerializeField] private SpaceLocator spaceLocator;
+
+    public UnityEvent<Pose, bool> onSpaceLocateSuccess;
+    public UnityEvent<Pose, bool> onSpaceLocateFailure;
+
+    private void Start()
     {
-        
+        spaceLocator.OnSpaceLocateCompleted.AddListener(
+            (pose, isValid) =>
+            {
+                if (isValid)
+                {
+                    onSpaceLocateSuccess?.Invoke(pose, true);
+                }
+                else
+                {
+                    onSpaceLocateFailure?.Invoke(pose, false);
+                }
+            });
     }
 }
