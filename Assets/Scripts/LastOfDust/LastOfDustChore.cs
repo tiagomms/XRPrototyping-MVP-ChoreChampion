986018b176eddbecc631c165199b0e5fd4888806
Tiago Silva <tiagomms.15@gmail.com>
2025-07-10 13:52:05 +0100

    timer countdown kind of working

--- Assets/Scripts/LastOfDust/LastOfDustChore.cs
+++ Assets/Scripts/LastOfDust/LastOfDustChore.cs
@@ -10,6 +10,7 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using System.Linq;
 using LastOfDust;
+using DG.Tweening;
 
 namespace Chores
 {
@@ -72,8 +73,15 @@ namespace Chores
             //randomMonsterSpawner.onSurfaceCleaned.AddListener(OnSurfaceCleaned);
             //randomMonsterSpawner.onAnchorCleaned.AddListener(OnAnchorCleaned);
             randomMonsterSpawner.onRoomCleaned.AddListener(OnRoomCleaned);
+            inGameUI.onCountDownFinished.AddListener(ShowMonsters);
 
             onChoreTimesUp.AddListener(OnTimesUp);
+        }
+
+        private void ShowMonsters()
+        {
+            timeElapsed = 0f;
+            //randomMonsterSpawner.transform.DOScale(Vector3.one, 0.2f).SetEase(Ease.OutBack);
         }
 
         protected virtual void OnDestroy()
@@ -81,6 +89,7 @@ namespace Chores
             //randomMonsterSpawner.onSurfaceCleaned.RemoveListener(OnSurfaceCleaned);
             //randomMonsterSpawner.onAnchorCleaned.RemoveListener(OnAnchorCleaned);
             randomMonsterSpawner.onRoomCleaned.RemoveListener(OnRoomCleaned);
+            inGameUI.onCountDownFinished.RemoveListener(ShowMonsters);
 
             onChoreTimesUp.RemoveListener(OnTimesUp);
         }
@@ -110,6 +119,7 @@ namespace Chores
             
             // all of them are spawned in 
             sceneNav.Agents = randomMonsterSpawner.gameObject.GetComponentsInChildren<NavMeshAgent>().ToList();
+            //randomMonsterSpawner.transform.localScale = Vector3.zero;
             /*
             allSlimes = randomMonsterSpawner.GetAllSpawnedObjects()
                 .Select(obj => obj.GetComponent<LodSlime>())
--- Assets/Scripts/LastOfDust/UI/Lod04HudInGameUI.cs
+++ Assets/Scripts/LastOfDust/UI/Lod04HudInGameUI.cs
@@ -8,12 +8,22 @@ using Oculus.Interaction;
 using System.Collections.Generic;
 using NaughtyAttributes;
 using TMPro;
+using UnityEngine.Events;
 
 namespace LastOfDust.UI
 {
     public class Lod04HudInGameUI : BaseUI
     {
         [Header("UI")]
+        [SerializeField] protected GameObject inGameUI;
+        [SerializeField] protected GameObject countdownSection;
+        [SerializeField] private TextMeshProUGUI countdownText;
+        [SerializeField] private AudioSource initialSound;
+        [SerializeField] private Animator countdownAnimator;
+        [SerializeField] private float countdownTime = 3f;
+
+
+        [SerializeField] protected GameObject timerScoreSection;
         [SerializeField] protected TMP_Text timerText;
         [SerializeField] protected TMP_Text scoreText;
 
@@ -24,13 +34,26 @@ namespace LastOfDust.UI
         [SerializeField] protected string timerStr = "00:00";
         [SerializeField] protected string scoreStr = "0";
 
+        private float _currentTime;
+        private bool _isCountingDown;
+
+        public UnityEvent onCountDownFinished;
+
+
         protected override void OnEnable()
         {
             base.OnEnable();
+            countdownAnimator.enabled = false;
+            //countdownSection.gameObject.SetActive(false);
+            //timerScoreSection.SetActive(false);
+
             if (LastOfDustChore.Instance == null) return;
+            StartCountDown();
+
             UpdateTimer(LastOfDustChore.Instance.ChoreCurrentTime);
             UpdateScore(LastOfDustChore.Instance.Score);
         }
+
         public void UpdateTimer(float newTime)
         {
             timerStr = string.Format("{0:00}:{1:00}", Mathf.FloorToInt(newTime / 60), Mathf.FloorToInt(newTime % 60));
@@ -51,7 +74,48 @@ namespace LastOfDust.UI
             }
         }
 
+        public void StartCountDown()
+        {
+            _isCountingDown = true;
+            _currentTime = countdownTime;
+            UIToggle(_isCountingDown);
+            countdownAnimator.enabled = true;
+
+            if (initialSound)
+            {
+                initialSound.Play();
+            }
+        }
+
+        private void UIToggle(bool isCountingDown)
+        {
+            countdownSection.gameObject.SetActive(isCountingDown);
+            timerScoreSection.SetActive(!isCountingDown);
+        }
+
+        private void OnCountdownComplete()
+        {
+            _isCountingDown = false;
+            _currentTime = 0f;
+            onCountDownFinished.Invoke();
+            countdownAnimator.enabled = false;
+
+            UIToggle(_isCountingDown);
+        }
+
         void Update()
+        {
+            if (_isCountingDown)
+            {
+                UpdateCountdownText();
+            }
+            else
+            {
+                UpdateInGameText();
+            }
+        }
+
+        private void UpdateInGameText()
         {
             if (LastOfDustChore.Instance.IsChoreActive)
             {
@@ -60,5 +124,15 @@ namespace LastOfDust.UI
             }
         }
 
+        private void UpdateCountdownText()
+        {
+            _currentTime -= Time.deltaTime;
+            countdownText.text = Mathf.CeilToInt(_currentTime).ToString();
+
+            if (_currentTime <= 0f)
+            {
+                OnCountdownComplete();
+            }
+        }
     }
 }
